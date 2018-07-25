f0e3f4e923f8194bb4548da01a311afb6331bcd7
Mikhail Temkine <miktemk@gmail.com>
2018-07-25 18:16:59 -0400

    ruin old proj

--- GenShortGUID/Generate.cs
+++ GenShortGUID/Generate.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿//------------------------------------------------------------------------------
+// <copyright file="Command1.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
 using System.ComponentModel.Design;
 using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Task = System.Threading.Tasks.Task;
 
-namespace GenShortGUID
+namespace VSIXProject1
 {
     /// <summary>
     /// Command handler
     /// </summary>
-    internal sealed class Generate
+    internal sealed class Command1
     {
         /// <summary>
         /// Command ID.
@@ -22,33 +25,40 @@ namespace GenShortGUID
         /// <summary>
         /// Command menu group (command set GUID).
         /// </summary>
-        public static readonly Guid CommandSet = new Guid("57acc539-654b-4fbc-8dcc-802938624ada");
+        public static readonly Guid CommandSet = new Guid("5411f545-cfa6-444b-a6c0-5cdc4f0eb32f");
 
         /// <summary>
         /// VS Package that provides this command, not null.
         /// </summary>
-        private readonly AsyncPackage package;
+        private readonly Package package;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Generate"/> class.
+        /// Initializes a new instance of the <see cref="Command1"/> class.
         /// Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
-        private Generate(AsyncPackage package, OleMenuCommandService commandService)
+        private Command1(Package package)
         {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
-            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
 
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(this.GenGUID, menuCommandID);
-            commandService.AddCommand(menuItem);
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
         }
 
         /// <summary>
         /// Gets the instance of the command.
         /// </summary>
-        public static Generate Instance
+        public static Command1 Instance
         {
             get;
             private set;
@@ -57,7 +67,7 @@ namespace GenShortGUID
         /// <summary>
         /// Gets the service provider from the owner package.
         /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        private IServiceProvider ServiceProvider
         {
             get
             {
@@ -69,22 +79,31 @@ namespace GenShortGUID
         /// Initializes the singleton instance of the command.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        public static async Task InitializeAsync(AsyncPackage package)
+        public static void Initialize(Package package)
         {
-            // Verify the current thread is the UI thread - the call to AddCommand in Generate's constructor requires
-            // the UI thread.
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
-            Instance = new Generate(package, commandService);
+            Instance = new Command1(package);
         }
 
-        private void GenGUID(object sender, EventArgs e)
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            string guid = Guid.NewGuid().ToString().Substring(8);
-            System.Diagnostics.Debug.WriteLine(guid);
-            Console.Write(guid);
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "Command1";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
         }
     }
 }
--- GenShortGUID/GeneratePackage.cs
+++ GenShortGUID/GeneratePackage.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿//------------------------------------------------------------------------------
+// <copyright file="Command1Package.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.InteropServices;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.Win32;
-using Task = System.Threading.Tasks.Task;
 
 namespace GenShortGUID
 {
@@ -32,22 +35,22 @@ namespace GenShortGUID
     /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </para>
     /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(GeneratePackage.PackageGuidString)]
+    [Guid(Command1Package.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class GeneratePackage : AsyncPackage
+    public sealed class Command1Package : Package
     {
         /// <summary>
-        /// GeneratePackage GUID string.
+        /// Command1Package GUID string.
         /// </summary>
-        public const string PackageGuidString = "377f4d80-29aa-42c5-a604-f0c8ee3eff51";
+        public const string PackageGuidString = "a32b2354-fb7b-4bc7-8371-3e1c92f89327";
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Generate"/> class.
+        /// Initializes a new instance of the <see cref="Command1"/> class.
         /// </summary>
-        public GeneratePackage()
+        public Command1Package()
         {
             // Inside this method you can place any initialization code that does not require
             // any Visual Studio service because at this point the package object is created but
@@ -61,15 +64,10 @@ namespace GenShortGUID
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        protected override void Initialize()
         {
-            // When initialized asynchronously, the current thread may be a background thread at this point.
-            // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            await Generate.InitializeAsync(this);
+            Command1.Initialize(this);
+            base.Initialize();
         }
 
         #endregion
